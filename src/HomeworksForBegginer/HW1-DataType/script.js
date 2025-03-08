39b1b3dab282880f1f3a7916ffe96e42352d2fcf
NikiRenegade <nikirenegade@gmail.com>
2025-03-08 17:02:09 +0700

    Сделаны тесты к первому ДЗ исправлены ошибки выявленные во врея тестов. Плюс добавились библиотеки

--- src/HomeworksForBegginer/HW1-DataType/script.js
+++ src/HomeworksForBegginer/HW1-DataType/script.js
@@ -9,11 +9,11 @@ function GetSumAndMultiplication(a, b){
 }
 
 function GetSymbolsCount(word1, word2){
-	return ("" + word1 + word2).split("").length;
+	return `${word1}${word2}`.length
 }
 
 function SomeThreeNumber(number){
-	return  isNaN(Number(number)) === true || number.split("").length !== 3 ? "Ошибка!" : number.split("").reduce((acc, el) => acc + Number(el), 0);
+	return  isNaN(Number(number)) === true ||  String(number).split("").length !== 3 ? "Ошибка!" : String(number).split("").reduce((acc, el) => acc + Number(el), 0);
 }
 
 module.exports = {
