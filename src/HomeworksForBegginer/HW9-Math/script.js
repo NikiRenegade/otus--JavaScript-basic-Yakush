001e54fd60e23dcf1c70d0c79e4f6b61e87a2406
NikiRenegade <nikirenegade@gmail.com>
2025-03-13 18:20:14 +0700

    Созданы тесты к ДЗ 9

---
+++ src/HomeworksForBegginer/HW9-Math/script.js
@@ -0,0 +1,17 @@
+function isRightTriangle(a, b, c) {
+	return null;
+}
+
+function GetAreaAndCircumferenceCircle(r) {
+	return null;
+}
+
+function GetRootsQuadraticEquation(a, b, c) {
+	return null;
+}
+
+module.exports = {
+	isRightTriangle,
+	GetAreaAndCircumferenceCircle,
+	GetRootsQuadraticEquation
+}
